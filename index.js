67a015dea699991dab3860e9aaadd524f5958e0e
LESANF <nagong1000@naver.com>
2020-02-13 00:15:20 +0900

    ignore node_modules

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 import { GraphQLServer } from "graphql-yoga";
+
+const server = new GraphQLServer({
+  typeDefs: "graphql/schema.graphql"
+});
+
+server.start(() => console.log("Graphql Server Running"));
