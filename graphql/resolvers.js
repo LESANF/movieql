397252b9d4eded5c9c831c29476120b201cbb9fc
LESANF <nagong1000@naver.com>
2020-02-14 05:26:58 +0900

    docs: END Part1

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,5 +1,11 @@
+import { getMovies, getMovie, getSuggestions } from "./db";
+
 const resolvers = {
   Query: {
-    name: () => "LESA"
+    movies: (_, { rating, limit }) => getMovies(limit, rating),
+    movie: (_, { id }) => getMovie(id),
+    suggestions: (_, { id }) => getSuggestions(id)
   }
 };
+
+export default resolvers;
